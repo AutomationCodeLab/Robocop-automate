7bf7d658aa0bcc0df99381d60b599c5d31d66540
AlfredAfutu <chatwithalfy@gmail.com>
2025-03-06 13:01:16 +0100

    Fix script

--- .github/scripts/automate-app-upload.sh
+++ .github/scripts/automate-app-upload.sh
@@ -6,10 +6,7 @@ APP_AUTOMATE_USER=$2
 APP_AUTOMATE_KEY=$3
 
 file=$(ls artifacts/app/*"$FILENAME")
-echo "Uploading file: $file"
-echo "User: $APP_AUTOMATE_USER"
-echo "Key: $APP_AUTOMATE_KEY"
-response=-$(curl -u "$APP_AUTOMATE_USER:$APP_AUTOMATE_KEY" \
+response=$(curl -u "$APP_AUTOMATE_USER:$APP_AUTOMATE_KEY" \
   -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
   -F "file=@$file")
 app_url=$(echo "$response" | jq -r '.app_url')
