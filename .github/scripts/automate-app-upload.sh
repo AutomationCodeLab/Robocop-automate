8f1328dd1951d78c12b719525b73dca2c32b5194
AlfredAfutu <chatwithalfy@gmail.com>
2025-03-06 12:39:58 +0100

    Debug output file not found

--- .github/scripts/automate-app-upload.sh
+++ .github/scripts/automate-app-upload.sh
@@ -6,8 +6,18 @@ APP_AUTOMATE_USER=$2
 APP_AUTOMATE_KEY=$3
 
 file=$(ls artifacts/app/*"$FILENAME")
+echo "User: $APP_AUTOMATE_USER"
+echo "Key: $APP_AUTOMATE_KEY"
 response=-$(curl -u "$APP_AUTOMATE_USER:$APP_AUTOMATE_KEY" \
   -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
   -F "file=@$file")
 app_url=$(echo "$response" | jq -r '.app_url')
-echo "::set-output name=app_url::$app_url"
+if [ -z "$GITHUB_OUTPUT" ]; then
+  echo "Error: GITHUB_OUTPUT is not set."
+  exit 1
+fi
+if [ ! -f "$GITHUB_OUTPUT" ]; then
+  echo "Error: $GITHUB_OUTPUT file does not exist."
+  exit 1
+fi
+echo "name=$app_url" >> "$GITHUB_OUTPUT"
