9cbae7717ec9e7e655d2bfa36cfbab0a62204935
AlfredAfutu <chatwithalfy@gmail.com>
2025-03-06 12:18:06 +0100

    Add automation workflow - testing

---
+++ .github/scripts/automate-app-upload.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Script to upload an application file to BrowserStack App Automate
+FILENAME=$1
+APP_AUTOMATE_USER=$2
+APP_AUTOMATE_KEY=$3
+
+file=$(ls artifacts/app/*"$FILENAME")
+response=-$(curl -u "$APP_AUTOMATE_USER:$APP_AUTOMATE_KEY" \
+  -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
+  -F "file=@$file")
+app_url=$(echo "$response" | jq -r '.app_url')
+echo "::set-output name=app_url::$app_url"
